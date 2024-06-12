bf9d400082a7b5bf34ce9dccba59a4acac089b6d
Mahmoud Alaskalany <mahmoud.ragab@dhofar.gov.om>
2024-06-12 09:54:54 +0400

    add palindrome check for number solution

--- Palindrome/C#/Palindrome-Number/Program.cs
+++ Palindrome/C#/Palindrome-Number/Program.cs
@@ -1,2 +1,20 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, Checking if Number is Palindrome Without Converting To String!");
+var strNumber = Console.ReadLine();
+int sum = 0;
+int remaining;
+int number;
+
+if (strNumber == null)
+{
+    return;
+}
+number = int.Parse(strNumber);
+int temp = number;
+while (number > 0)
+{
+    remaining = number % 10; //example 112211 this will give 11221.1 1 is the remaining 
+    sum = (sum * 10) + remaining; // this will take 0 * 10 = 0 + 1 the remaining = 1
+    number = number / 10; // 112211 /10 = 11221.1 and since this is integer it will be 11221 then next loop will be 11221 % 10 which give 1122.1
+}
+Console.WriteLine(temp == sum ? "Number Is Palindrome !" : "Number Is Not Palindrome!!");
