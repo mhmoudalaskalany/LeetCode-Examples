fa25d581e3b60b5f28c8015c9fb809d1097efc7c
Mahmoud Alaskalany <mahmoud.ragab@dhofar.gov.om>
2024-06-12 08:59:19 +0400

    add palindrome for loop solution

---
+++ Palindrome/C#/Palindrome-Forloop/Program.cs
@@ -0,0 +1,17 @@
+﻿var str = Console.ReadLine();
+if(str == null)
+{
+    return;
+}
+var length = str.Length;
+var isPalindrome = true;
+for(int i = 0 ; i < length / 2 ; i ++)
+{
+    if(str[i] != str[length - 1 - i])
+    {
+        isPalindrome = false;
+        break;
+    }
+}
+
+Console.WriteLine(isPalindrome ? "Is Palindrome" : "Not Palindrome");
